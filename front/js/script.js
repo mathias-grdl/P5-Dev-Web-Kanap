c9d20573f4966b2c8df994c9998fb4a202e25a64
mathias-grdl <114570081+mathias-grdl@users.noreply.github.com>
2022-12-06 22:31:01 +0100

    fetch
    récupération des produits dans la console

--- front/js/script.js
+++ front/js/script.js
@@ -1,3 +1,11 @@
 // https://openclassrooms.com/fr/courses/5543061-ecrivez-du-javascript-pour-le-web/5577591-recuperez-des-donnees-dun-service-web
+
+// https://www.youtube.com/watch?v=b0dPBK37-M8&t=790s
+
+
+// Contact l'url et on récupère une promesse réponse (.then) -> response.json formate la réponse et nous fait une nouvelle promesse. Et on refait un .then pour récupérer le résultat.
+
 fetch("http://localhost:3000/api/products")
-    .then ((res) => res.json())
+    .then(response => response.json())
+    .then(data => console.log(data));
+
