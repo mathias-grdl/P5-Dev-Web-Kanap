a2d70f38bc16e6aa64f32df5aed97e52e5cb2a38
mathias-grdl <114570081+mathias-grdl@users.noreply.github.com>
2023-01-09 21:39:51 +0100

    Update cart.js

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,7 +1,6 @@
-
-
 const Cart = []
 // numberProductObject()
+
 
 // récupération des products dans le cart
 function numberProductObject() {
@@ -10,8 +9,9 @@ function numberProductObject() {
     for (let i = 0; i <= numberProducts; i++) {
         const products = localStorage.getItem(localStorage.key(i));
         const productObject = JSON.parse(products); //passage de string à objet
+        // https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Array/push
         Cart.push(productObject)
-        console.log(i, product, productObject); 
+        // console.log(i, product, productObject); 
     }
 }
 
