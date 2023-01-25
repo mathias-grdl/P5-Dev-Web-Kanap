7e5d8e2483b9ae27c1285bb8f5d7be3475fcdcb1
mathias-grdl <114570081+mathias-grdl@users.noreply.github.com>
2023-01-25 11:28:18 +0100

    Update cart.js

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,4 +1,5 @@
 let productArray = JSON.parse(localStorage.getItem("product"));
+
 if (localStorage.length === 0 || productArray.length === 0) {
     document.querySelector('#cart__items').innerHTML = "Votre panier est vide";
 } else {
@@ -7,6 +8,93 @@ if (localStorage.length === 0 || productArray.length === 0) {
         .then(response => response.json())
         .then(product => {
             // console.table(product);
-
+            // CreateHTML(product);
+            displayProduct();
         })
+    }
+
+
+  function displayProduct(){
+    for (let i=0; i < productArray.length; i++) {
+
+        // Création de la balise "article" et insertion dans la section
+        let productArticle = document.createElement("article");
+        document.querySelector("#cart__items").appendChild(productArticle);
+        productArticle.className = "cart__item";
+        productArticle.setAttribute("data-id", productArray[i].id);
+
+        // Insertion de l'élément "div" pour l'image produit
+        let productDivImg = document.createElement("div");
+        productArticle.appendChild(productDivImg);
+        productDivImg.className = "cart__item__img";
+
+        // Insertion de l'image
+        let productImg = document.createElement("img");
+        productDivImg.appendChild(productImg);
+        productImg.src = productArray[i].imageUrl;
+        // productImg.alt = productArray.altTxt;
+        
+        // Insertion de l'élément "div" pour la description produit
+        let productItemContent = document.createElement("div");
+        productArticle.appendChild(productItemContent);
+        productItemContent.className = "cart__item__content";
+
+        // Insertion de l'élément "div"
+        let productItemContentTitlePrice = document.createElement("div");
+        productItemContent.appendChild(productItemContentTitlePrice);
+        productItemContentTitlePrice.className = "cart__item__content__titlePrice";
+        
+        // Insertion du titre h2
+        let productTitle = document.createElement("h2");
+        productItemContentTitlePrice.appendChild(productTitle);
+        productTitle.innerHTML = productArray[i].name;
+
+        // Insertion de la couleur
+        let productColor = document.createElement("p");
+        productTitle.appendChild(productColor);
+        productColor.innerHTML = productArray[i].color;
+        productColor.style.fontSize = "20px";
+        
+
+        // Insertion du prix
+        let productPrice = document.createElement("p");
+        productItemContentTitlePrice.appendChild(productPrice);
+        productPrice.innerHTML = productArray[i].price + " €";
+ 
+        // Insertion de l'élément "div"
+        let productItemContentSettings = document.createElement("div");
+        productItemContent.appendChild(productItemContentSettings);
+        productItemContentSettings.className = "cart__item__content__settings";
+ 
+        // Insertion de l'élément "div"
+        let productItemContentSettingsQuantity = document.createElement("div");
+        productItemContentSettings.appendChild(productItemContentSettingsQuantity);
+        productItemContentSettingsQuantity.className = "cart__item__content__settings__quantity";
+        
+        // Insertion de "Qté : "
+        let productQty = document.createElement("p");
+        productItemContentSettingsQuantity.appendChild(productQty);
+        productQty.innerHTML = "Qté : ";
+        
+        // Insertion de la quantité
+        let productQuantity = document.createElement("input");
+        productItemContentSettingsQuantity.appendChild(productQuantity);
+        productQuantity.value = productArray[i].quantity;
+        productQuantity.className = "itemQuantity";
+        productQuantity.setAttribute("type", "number");
+        productQuantity.setAttribute("min", "1");
+        productQuantity.setAttribute("max", "100");
+        productQuantity.setAttribute("name", "itemQuantity");
+        
+        // Insertion de l'élément "div"
+        let productItemContentSettingsDelete = document.createElement("div");
+        productItemContentSettings.appendChild(productItemContentSettingsDelete);
+        productItemContentSettingsDelete.className = "cart__item__content__settings__delete";
+
+        // Insertion de "p" supprimer
+        let productSupprimer = document.createElement("p");
+        productItemContentSettingsDelete.appendChild(productSupprimer);
+        productSupprimer.className = "deleteItem";
+        productSupprimer.innerHTML = "Supprimer";
+  }
 }
